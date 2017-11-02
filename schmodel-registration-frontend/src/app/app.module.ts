c103058ff17a7d2e0b4caff83a6d71d41b20e9e0
BearandYoon <luckysevenbear@gmail.com>
2017-11-03 03:21:06 +0800

    Update app module
    * Add auth guard module to app module
    * Add basic necessary modules

--- schmodel-registration-frontend/src/app/app.module.ts
+++ schmodel-registration-frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
