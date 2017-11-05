b4e9c8135c47fcf87022690ed1fe3f0a62c3291f
BearandYoon <luckysevenbear@gmail.com>
2017-11-05 18:37:11 +0100

    added validation service

--- schmodel-registration-frontend/src/app/pages/login/login.module.ts
+++ schmodel-registration-frontend/src/app/pages/login/login.module.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { ValidationService } from '../../shared/services';
+import { ControlMessagesComponent } from "../../components";
+
 @NgModule({
-    imports: [
-        CommonModule,
-        LoginRoutingModule
-    ],
-    declarations: [LoginComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoginRoutingModule
+  ],
+  declarations: [
+    LoginComponent,
+    ControlMessagesComponent
+  ],
+  providers: [
+    ValidationService,
+  ],
 })
 export class LoginModule {
 }
