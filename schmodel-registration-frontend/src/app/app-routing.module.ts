b025359cd317737ffe939decbf19aa14faf4f8cd
BearandYoon <luckysevenbear@gmail.com>
2017-11-05 22:21:45 +0100

    improve routing

--- schmodel-registration-frontend/src/app/app-routing.module.ts
+++ schmodel-registration-frontend/src/app/app-routing.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared';
+import { LayoutComponent } from './pages/layout';
 
 const routes: Routes = [
-    {
-        path: '',
-        loadChildren: './pages/home/home.module#HomeModule',
-        canActivate: [AuthGuard]
-    },
-    { path: 'login', loadChildren: './pages/login/login.module#LoginModule' },
-    { path: 'signup', loadChildren: './pages/signup/signup.module#SignupModule' },
-    { path: 'edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfileModule' },
-    { path: 'not-found', loadChildren: './pages/not-found/not-found.module#NotFoundModule' },
-    { path: '**', redirectTo: 'not-found' }
+  { path: 'login', loadChildren: './pages/login/login.module#LoginModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupModule' },
+  { path: 'not-found', loadChildren: './pages/not-found/not-found.module#NotFoundModule' },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '', loadChildren: './pages/home/home.module#HomeModule'
+      }, {
+        path: 'edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfileModule'
+      }
+    ]
+  },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
