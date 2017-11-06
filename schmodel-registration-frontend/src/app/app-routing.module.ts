7aa7f4e4b09852e5e8eb4db99812c36513f75be6
BearandYoon <luckysevenbear@gmail.com>
2017-11-06 23:46:59 +1000

    initial first row

--- schmodel-registration-frontend/src/app/app-routing.module.ts
+++ schmodel-registration-frontend/src/app/app-routing.module.ts
@@ -1,27 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared';
-import { LayoutComponent } from './pages/layout/layout.component';
+import { MyJobsComponent } from './pages/my-jobs/my-jobs.component';
 
 
 const routes: Routes = [
     {
         path: '',
-        loadChildren: './pages/my-jobs/my-jobs.module#MyJobsModule',
+        loadChildren: './pages/home/home.module#HomeModule',
         canActivate: [AuthGuard]
     },
     { path: 'login', loadChildren: './pages/login/login.module#LoginModule' },
     { path: 'signup', loadChildren: './pages/signup/signup.module#SignupModule' },
     { path: 'not-found', loadChildren: './pages/not-found/not-found.module#NotFoundModule' },
-    // {
-    //     path: '',
-    //     component: LayoutComponent,
-    //     canActivate: [AuthGuard],
-    //     children: [
-    //       { path: '', loadChildren: './pages/home/home.module#HomeModule' },
-    //       { path: 'my-jobs', loadChildren: './pages/my-jobs/my-jobs.module#MyJobsModule' }
-    //     ]
-    //   },
+    { path: 'my-jobs', loadChildren: './pages/my-jobs/my-jobs.module#MyJobsModule' },
     { path: '**', redirectTo: 'not-found' }
 ];
 
