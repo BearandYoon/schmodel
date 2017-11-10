52fb4342fb6c694386b1db69950fa941cfc28fa3
BearandYoon <luckysevenbear@gmail.com>
2017-11-10 19:23:51 +1000

    intial footer on 11th page

--- schmodel-registration-frontend/src/app/pages/my-jobs/components/event/event.component.ts
+++ schmodel-registration-frontend/src/app/pages/my-jobs/components/event/event.component.ts
@@ -11,7 +11,7 @@ import { routerTransition } from '../../../../router.animations';
 
 export class EventComponent implements OnInit {
 
-  private light_blue: string = 'rgba(78, 200, 235, 0.1)';
+  private light_blue:string = 'rgba(78, 200, 235, 0.1)';
   private light_purple:string = 'rgba(53, 20, 77, 0.1)';
 
   public race_bg_urls: Array<any> = [];
@@ -41,20 +41,20 @@ export class EventComponent implements OnInit {
   }
 
   ngOnChanges() {
-    this.bg_url = this.race_bg_urls[this.race_no].imagePath;
-    if(this.race_no % 2 == 0)
+    this.bg_url = this.race_bg_urls[this.event_no].imagePath;
+    if(this.event_no % 2 == 0)
       this.title_border_style = this.light_blue;
     else
       this.title_border_style = this.light_purple;
   }
   
   @Output() event: EventEmitter<any> = new EventEmitter();
-  @Input() race_no: number;               //only for identify the row
-  @Input() race_number: string;           //formula race number e.g : R1-2, R3, R4, ....
-  @Input() race_date: string;             //formula race date   e.g : Dec. 2-3, 2017,  Jan. 13, 2018, ...
-  @Input() race_city: string;             //formula race city   e.g : MARRAKESH, SANTIAGO, MEXICO CITY, ...
-  @Input() race_country_code: string;     //formula race country code e.g : CL, MX, BR, ...
+  @Input() event_no: number;               //only for identify the row
+  @Input() event_number: string;           //formula race number e.g : R1-2, R3, R4, ....
+  @Input() event_date: string;             //formula race date   e.g : Dec. 2-3, 2017,  Jan. 13, 2018, ...
+  @Input() event_city: string;             //formula race city   e.g : MARRAKESH, SANTIAGO, MEXICO CITY, ...
+  @Input() event_country_code: string;     //formula race country code e.g : CL, MX, BR, ...
 
-  @Input() job_price: string[];           //[job_price] = "['1,400€', '200€', '0', '0']"
-  @Input() job_status: number[];          //[job_status] = "[1, 2, 0, 0]", 0:disabled, 1:checked, 2:apply
+  @Input() event_role_price: string[];           //[job_price] = "['1,400€', '200€', '0', '0']"
+  @Input() event_role_status: number[];          //[job_status] = "[1, 2, 0, 0]", 0:disabled, 1:checked, 2:apply
 }
