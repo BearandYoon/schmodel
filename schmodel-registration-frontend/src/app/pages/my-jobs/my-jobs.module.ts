3d0f9d4235ff5491f4a327f53a083ca83bd5d8fb
BearandYoon <luckysevenbear@gmail.com>
2017-11-10 11:26:23 +1000

    fix issue at my-jobs.module

--- schmodel-registration-frontend/src/app/pages/my-jobs/my-jobs.module.ts
+++ schmodel-registration-frontend/src/app/pages/my-jobs/my-jobs.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { MyJobsRoutingModule } from './my-jobs-routing.module';
 import { MyJobsComponent } from './my-jobs.component';
-import { MyJobsButtonComponent } from './components/my-jobs-button/my-jobs-button.component';
-import { FormulaRaceComponent } from './components/formula-race/formula-race.component';
 import { EventRoleComponent } from './components/event-role/event-role.component';
 import { EventComponent } from './components/event/event.component';
 
@@ -13,6 +11,6 @@ import { EventComponent } from './components/event/event.component';
     CommonModule,
     MyJobsRoutingModule
   ],
-  declarations: [MyJobsComponent, MyJobsButtonComponent, FormulaRaceComponent, EventRoleComponent, EventComponent]
+  declarations: [MyJobsComponent, EventRoleComponent, EventComponent]
 })
 export class MyJobsModule { }
