385153d32d9852f002abaf65318d3dd8fc5b0df0
BearandYoon <luckysevenbear@gmail.com>
2017-11-10 09:14:27 +0100

    implemented basic accordion

--- schmodel-registration-frontend/src/app/pages/edit-profile/edit-profile.component.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.scss']
+  styleUrls: ['./edit-profile.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EditProfileComponent implements OnInit {
+
+  panels: any[] = [
+    {
+      title: 'PASSWORD',
+      isOpen: false
+    },
+    {
+      title: 'PHOTOS',
+      isOpen: false
+    },
+    {
+      title: 'PERSONAL INFORMATION',
+      isOpen: false
+    },
+    {
+      title: 'MY TERMS & CONDITIONS',
+      isOpen: false
+    },
+    {
+      title: 'BILLING INFORMATION',
+      isOpen: false
+    }
+  ]
 
   constructor() { }
 
--- schmodel-registration-frontend/src/app/pages/edit-profile/edit-profile.module.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-profile.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AccordionModule } from 'ngx-bootstrap';
 
 import { EditProfileRoutingModule } from './edit-profile-routing.module';
 import { EditProfileComponent } from './edit-profile.component';
@@ -7,6 +8,7 @@ import { EditProfileComponent } from './edit-profile.component';
 @NgModule({
   imports: [
     CommonModule,
+    AccordionModule.forRoot(),
     EditProfileRoutingModule
   ],
   declarations: [EditProfileComponent]
