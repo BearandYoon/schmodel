c64cb0ea19bba3c6055296a13bd611d467b457d1
BearandYoon <luckysevenbear@gmail.com>
2017-11-13 09:08:57 +0100

    Add section skeletons on EditProfile

---
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-billing-info/edit-billing-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'edit-billing-info',
+  templateUrl: './edit-billing-info.component.html',
+  styleUrls: ['./edit-billing-info.component.scss']
+})
+export class EditBillingInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
