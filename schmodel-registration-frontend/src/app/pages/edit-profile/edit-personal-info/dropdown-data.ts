1cdf25db1008782c695a8bcdd8cdd3b1c40fe8a8
BearandYoon <luckysevenbear@gmail.com>
2017-11-15 20:06:02 +0100

    dropdown improvement

--- schmodel-registration-frontend/src/app/pages/edit-profile/edit-personal-info/dropdown-data.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-personal-info/dropdown-data.ts
@@ -1,53 +1,162 @@
-export const month = {
+const months = {
   placeholder: 'MM',
+  options: []
+};
+
+for (let i = 1; i <= 12; i ++) {
+  months.options.push({
+    value: i,
+    text: i
+  })
+}
+
+const days = {
+  placeholder: 'DD',
+  options: []
+};
+
+for (let i = 1; i <= 31; i ++) {
+  days.options.push({
+    value: i,
+    text: i
+  })
+}
+
+const years = {
+  placeholder: 'YYYY',
+  options: []
+};
+
+for (let i = 1970; i <= 2000; i ++) {
+  years.options.push({
+    value: i,
+    text: i
+  })
+}
+
+const countries = {
+  placeholder: '',
   options: [
     {
-      value: '1',
-      text: '1'
-    },
-    {
-      value: '2',
-      text: '2'
-    },
-    {
-      value: '3',
-      text: '3'
-    },
-    {
-      value: '4',
-      text: '4'
-    },
-    {
-      value: '5',
-      text: '5'
-    },
-    {
-      value: '6',
-      text: '6'
-    },
-    {
-      value: '7',
-      text: '7'
-    },
-    {
-      value: '8',
-      text: '8'
-    },
-    {
-      value: '9',
-      text: '9'
-    },
-    {
-      value: '10',
-      text: '10'
-    },
-    {
-      value: '11',
-      text: '11'
-    },
-    {
-      value: '12',
-      text: '12'
-    },
+      value: 'United Kingdom',
+      text: 'United Kingdom'
+    }
   ]
+};
+
+const phonePrefixes = {
+  placeholder: '',
+  options: [
+    {
+      value: '41',
+      text: '(+41) UK'
+    }
+  ]
+};
+
+const nationalities = {
+  placeholder: '',
+  options: [
+    {
+      value: 'British',
+      text: 'British'
+    }
+  ]
+};
+
+const languages = {
+  placeholder: '',
+  options: [
+    {
+      value: 'English',
+      text: 'English'
+    }
+  ]
+};
+
+const heights = {
+  placeholder: '',
+  options: [
+    {
+      value: 177,
+      text: '5\'10.0" (177cm)'
+    }
+  ]
+};
+
+const chestSizes = {
+  placeholder: '',
+  options: [
+    {
+      value: '34B',
+      text: '34B'
+    }
+  ]
+};
+
+const waistSizes = {
+  placeholder: '',
+  options: [
+    {
+      value: 78,
+      text: '31" (78 cm)'
+    }
+  ]
+};
+
+const weights = {
+  placeholder: '',
+  options: [
+    {
+      value: 121,
+      text: '121 (55kg)'
+    }
+  ]
+};
+
+const dressSizes = {
+  placeholder: '',
+  options: [
+    {
+      value: 5.5,
+      text: '5.5 (36.0 EU)'
+    }
+  ]
+};
+
+const hairColors = {
+  placeholder: '',
+  options: [
+    {
+      value: 'Blond',
+      text: 'Blond'
+    }
+  ]
+};
+
+const eyeColors = {
+  placeholder: '',
+  options: [
+    {
+      value: 'Green',
+      text: 'Green'
+    }
+  ]
+};
+
+export {
+  months,
+  days,
+  years,
+  countries,
+  phonePrefixes,
+  nationalities,
+  languages,
+  heights,
+  chestSizes,
+  waistSizes,
+  weights,
+  dressSizes,
+  hairColors,
+  eyeColors
 };
