b8eb290d7d33dc65b0b529c704599adc92c3326d
BearandYoon <luckysevenbear@gmail.com>
2017-11-19 01:32:39 +0100

    issue #25 - Make the Activationcode optional in SignUp page * replace validation Msg

--- schmodel-registration-frontend/src/app/shared/services/validationService/validation.service.ts
+++ schmodel-registration-frontend/src/app/shared/services/validationService/validation.service.ts
@@ -4,9 +4,8 @@ export class ValidationService {
   static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
     const config = {
       'required': 'Required',
-      'invalidEmailAddress': 'Invalid email address',
-      'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
-      'invalidPhoneNumber': 'Invalid Phone Number',
+      'invalidEmailAddress': ValidationMessage.INVALID_EMAIL,
+      'invalidPassword': ValidationMessage.INVALID_PASSWORD,
       'minlength': `Minimum length ${validatorValue.requiredLength}`
     };
     return config[validatorName];
