84106d4329b8578461dabc5cd959a5a0763ad2b2
BearandYoon <luckysevenbear@gmail.com>
2017-11-20 21:52:48 +1000

    talent-photos

--- schmodel-registration-frontend/src/app/pages/edit-profile/edit-talent-photos/edit-talent-photos.component.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-talent-photos/edit-talent-photos.component.ts
@@ -11,24 +11,31 @@ export class EditTalentPhotosComponent implements OnInit {
   @ViewChild('fileInput') fileInput:ElementRef;
 
   public photo_section_infor: Array<any> = [];
-  public url: string = "";
-
+  public no_tmp: number;
+  public height: number;
+  public width: number;
   constructor(private renderer:Renderer) { }
 
   ngOnInit() {
-    this.url = "/assets/images/bg_cell_gradient.png";
+    this.no_tmp = 0;
     this.photo_section_infor.push(
-      {text:"Headshot Photo",    flag: false, backImg:"/assets/images/ic_photo_head.svg", backSize: '80% auto', backPos: '50% 100%'},
-      {text:"Full Length Photo", flag: false, backImg:"/assets/images/ic_photo_fullbody.svg", backSize: '35% auto', backPos: '50% 50%'},
-      {text:"Profile Photo",     flag: false, backImg:"/assets/images/ic_photo_profile.svg", backSize: '65% auto', backPos: '50% 100%'},
-      {text:"Additional Photo",  flag: false, backImg:"", backSize: "", backPos: ""},
-      {text:"Additional Photo",  flag: false, backImg:"", backSize: "", backPos: ""},
-      {text:"Additional Photo",  flag: false, backImg:"", backSize: "", backPos: ""},
+      {text:"Headshot Photo",    url:"", flag: false, backImg:"/assets/images/ic_photo_head.svg", backSize: '80% auto', backPos: '50% 100%'},
+      {text:"Full Length Photo", url:"", flag: false, backImg:"/assets/images/ic_photo_fullbody.svg", backSize: '35% auto', backPos: '50% 50%'},
+      {text:"Profile Photo",     url:"", flag: false, backImg:"/assets/images/ic_photo_profile.svg", backSize: '65% auto', backPos: '50% 100%'},
+      {text:"Additional Photo",  url:"", flag: false, backImg:"", backSize: "", backPos: ""},
+      {text:"Additional Photo",  url:"", flag: false, backImg:"", backSize: "", backPos: ""},
+      {text:"Additional Photo",  url:"", flag: false, backImg:"", backSize: "", backPos: ""},
     );
   }
 
-  onUploadFile() {
+  trackByFn(index: any, item: any) {
+    return index;
+  }
+
+  onUploadFile(num: number) {
     let event = new MouseEvent('click', {bubbles: true});
+    this.no_tmp = num;
+
     this.renderer.invokeElementMethod(
       this.fileInput.nativeElement, 'dispatchEvent', [event]);
   }
@@ -38,7 +45,8 @@ export class EditTalentPhotosComponent implements OnInit {
       let reader = new FileReader();
 
       reader.onload = (event:any) => {
-        this.url = event.target.result;
+        this.photo_section_infor[this.no_tmp].url = event.target.result;
+        this.photo_section_infor[this.no_tmp].flag = true;
       }
 
       reader.readAsDataURL(event.target.files[0]);
