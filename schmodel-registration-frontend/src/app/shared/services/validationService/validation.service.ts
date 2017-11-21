28b743e272aabfa4cc9d974999fdd316dc1cfafa
BearandYoon <luckysevenbear@gmail.com>
2017-11-21 16:04:13 +0100

    issue #23 - Merge SignUp Login Home ResetPw Edit my profile * Fix validation message * Add some text for scroll testing

--- schmodel-registration-frontend/src/app/shared/services/validationService/validation.service.ts
+++ schmodel-registration-frontend/src/app/shared/services/validationService/validation.service.ts
@@ -1,13 +1,11 @@
 import { ValidationMessage } from '../../../shared/models';
 
 export class ValidationService {
-  static getValidatorErrorMessage(validatorName: string, validatorValue: any = {}) {
+  static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
     const config = {
       'required': 'Required',
-      'invalidEmailAddress': 'Invalid email address',
-      'invalidPassword': 'Password must be at least 6 characters and contain a number.',
-      'passwordNotMatch': 'Passwords do not match.',
-      'invalidPhoneNumber': 'Invalid Phone Number',
+      'invalidEmailAddress': ValidationMessage.INVALID_EMAIL,
+      'invalidPassword': ValidationMessage.INVALID_PASSWORD,
       'minlength': `Minimum length ${validatorValue.requiredLength}`
     };
     return config[validatorName];
