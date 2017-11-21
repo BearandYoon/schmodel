e92569519829b4da18f8b90d76c78447f77bb4d9
BearandYoon <luckysevenbear@gmail.com>
2017-11-22 03:07:13 +1000

    file size limit: 5MB

--- schmodel-registration-frontend/src/app/pages/edit-profile/edit-talent-photos/edit-talent-photos.component.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-talent-photos/edit-talent-photos.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewChild, Renderer, ElementRef } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
 import { ProfileService } from '../../../core/services';
+import { MessageModalComponent } from '../../../shared/modules/messageModal/messageModal.component';
 
 @Component({
   selector: 'edit-talent-photos',
@@ -16,9 +20,21 @@ export class EditTalentPhotosComponent implements OnInit {
   public photo_section_infor: Array<any> = [];
   public response: Array<any> = [];
   public no_tmp: number;
+  public myFile: number;
   private data: any;
 
-  constructor(private renderer:Renderer, private profileService: ProfileService) {
+  messageModalRef: BsModalRef;
+  messageContent: string;
+  messageModalConfig = {
+    animated: true,
+    keyboard: true,
+    backdrop: true,
+    ignoreBackdropClick: false
+  };
+
+  constructor(private renderer:Renderer,
+              private profileService: ProfileService,
+              private dlgService: BsModalService) {
   }
 
   ngOnInit() {
@@ -52,8 +68,8 @@ export class EditTalentPhotosComponent implements OnInit {
       } 
     }
     console.log(this.photo_section_infor);
-
-    this.no_tmp = 0;    
+    this.messageContent = "We can only accept photos smaller than 5MB. Please select another photo and try again.";
+    this.no_tmp = -1;    
     this.data = {file: "", photoTypeId: 0, photoWidth: 0, photoHeight: 0};
   }
 
@@ -74,12 +90,14 @@ export class EditTalentPhotosComponent implements OnInit {
   onUploadFile(num: number) {
     let event = new MouseEvent('click', {bubbles: true});
     this.no_tmp = num;
+    this.myFile = null;
 
     this.renderer.invokeElementMethod(
       this.fileInput.nativeElement, 'dispatchEvent', [event]);
   }
 
   valuechange(event: any) {
+    this.myFile = event.nativeElement;
     if(event.target.files && event.target.files[0]) {
       let reader = new FileReader();
       this.data.file = event.target.files[0];
@@ -88,23 +106,30 @@ export class EditTalentPhotosComponent implements OnInit {
         let imgWidth, imgHeight, image = new Image();
         image.src = event.target.result;
         image.onload = function () {
-          this.photo_section_infor[this.no_tmp].flag = true;
-          this.data.photoWidth = image.width;
-          this.data.photoHeight = image.height;
-          this.data.photoTypeId = this.photo_section_infor[this.no_tmp].photoTypeId;
+          if(this.data.file.size >= 5*1024*1024) {
+            this.messageModalRef = this.dlgService.show(MessageModalComponent, this.messageModalConfig);
+            this.messageModalRef.content.messageContent = this.messageContent;
+            this.messageModalRef.content.isBtnCancel = false;        
+            this.messageModalRef.content.onCloseReason.subscribe(result => {});
+          } else {
+            this.photo_section_infor[this.no_tmp].flag = true;
+            this.data.photoWidth = image.width;
+            this.data.photoHeight = image.height;
+            this.data.photoTypeId = this.photo_section_infor[this.no_tmp].photoTypeId;
 
-          this.profileService.uploadPhoto(
-            this.data.file, 
-            this.data.photoTypeId, 
-            this.data.photoWidth, 
-            this.data.photoHeight).subscribe( res => {
-              console.log(res);
-              this.photo_section_infor[this.no_tmp].photoUrl = res.photoUrl;
-              this.photo_section_infor[this.no_tmp].photoId = res.photoId;
-              this.photo_section_infor[this.no_tmp].photo = res.photoUrl;
-          }, error => {
-            console.log(error);
-          });
+            this.profileService.uploadPhoto(
+              this.data.file, 
+              this.data.photoTypeId, 
+              this.data.photoWidth, 
+              this.data.photoHeight).subscribe( res => {
+                console.log(res);
+                this.photo_section_infor[this.no_tmp].photoUrl = res.photoUrl;
+                this.photo_section_infor[this.no_tmp].photoId = res.photoId;
+                this.photo_section_infor[this.no_tmp].photo = res.photoUrl;
+            }, error => {
+              console.log(error);
+            });
+          }
         }.bind(this);
       }
       reader.readAsDataURL(event.target.files[0]);
