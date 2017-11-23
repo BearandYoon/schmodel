5eee301c569ff2fc9264da80d68f43c4145b3322
BearandYoon <luckysevenbear@gmail.com>
2017-11-23 15:56:38 +0100

    citizenship language avoid selecting value multiple times

--- schmodel-registration-frontend/src/app/pages/edit-profile/sch-dropdown-row/sch-dropdown-row.component.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/sch-dropdown-row/sch-dropdown-row.component.ts
@@ -23,6 +23,7 @@ export class SchDropdownRowComponent implements ControlValueAccessor {
   onChange: any = () => { };
   onTouched: any = () => { };
   values: any = [];
+  excludedValues: any = [];
 
   get value() {
     return this._value;
@@ -66,18 +67,39 @@ export class SchDropdownRowComponent implements ControlValueAccessor {
     }
   }
 
+  adjustLists() {
+    if (!this.allowAddCategory) {
+      return;
+    }
+
+    const excludedValues = [];
+    for (let i = 0; i < this.items.length; i ++) {
+      const newValues = this.values.slice();
+      if (i < newValues.length) {
+        newValues.splice(i, 1);
+      }
+      excludedValues.push(newValues);
+    }
+
+    this.excludedValues = excludedValues;
+  }
+
   onValueChange(value, index) {
     while (this.values.length <= index) {
       this.values.push('');
     }
     this.values[index] = value;
     this.value = this.values.join('|');
+
+    this.adjustLists();
   }
 
   onClickAdd() {
     const newArray = this.items.slice();
     newArray.push(this.allowAddCategory);
     this.items = newArray;
+
+    this.adjustLists();
   }
 
 }
