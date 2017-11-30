25cc683e53f0ff68ed2961a767de28f85ede4704
BearandYoon <luckysevenbear@gmail.com>
2017-11-30 16:09:57 +0100

    #67-implement-client-event-calendar-page
    ## added authentication header

--- schmodel-registration-frontend/src/app/app-routing.module.ts
+++ schmodel-registration-frontend/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'edit-profile',
@@ -30,7 +30,7 @@ const routes: Routes = [
       },
       {
         path: 'event-calendar',
-        // canActivate: [AuthGuard],
+        canActivate: [AuthGuard],
         loadChildren: './pages/event-calendar/event-calendar.module#EventCalendarModule',
         data: {
           title: 'Event Calendar',
