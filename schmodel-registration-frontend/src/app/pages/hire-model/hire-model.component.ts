e13d91bce8cd96da3052b99a9ed3656e16a938c9
BearandYoon <luckysevenbear@gmail.com>
2017-11-30 17:34:54 -0400

    static data integration

--- schmodel-registration-frontend/src/app/pages/hire-model/hire-model.component.ts
+++ schmodel-registration-frontend/src/app/pages/hire-model/hire-model.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
+import hireModelData from './data';
 
 @Component({
   selector: 'app-hire-model',
@@ -20,14 +21,56 @@ export class HireModelComponent implements OnInit {
     ignoreBackdropClick: true
   };
 
+  hireModelData: any = {};
+
   constructor(
     private modalService: BsModalService,
-  ) { }
+  ) {
+    this.transformData(hireModelData);
+  }
 
   ngOnInit() {
     let pageTitleDom = document.getElementById('page-title');
     pageTitleDom.style.fontSize = '14px';
     pageTitleDom.innerHTML = '<strong>R3</strong> | Jan. 13, 2018 | <strong>MARRAKESH</strong>, MA';
+  }
+
+  transformData(data) {
+    for (const role of data.roles) {
+      role.applicants = 0;
+      role.liked = 0;
+      role.hired = 0;
+      for (const talent of data.talents) {
+        for (const application of talent.applications) {
+          if (application.roleId === role.id) {
+            role.applicants ++;
+            if (application.liked) {
+              role.liked ++;
+              if (talent.hired) {
+                role.hired ++;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    for (const talent of data.talents) {
+      talent.roles = [];
+      for (const role of data.roles) {
+        const newRole = Object.assign({}, role);
+        talent.roles.push(newRole);
+        const applications = talent.applications.filter(item => item.roleId === newRole.id);
+        if (applications && applications.length) {
+          newRole.application = applications[0];
+        }
+      }
+    }
+    this.hireModelData = data;
+
+    // add empty column
+    data.roles.push({});
+    console.log(this.hireModelData);
   }
 
   confirmHiring() {
--- schmodel-registration-frontend/src/app/pages/hire-model/talent-item/talent-item.component.ts
+++ schmodel-registration-frontend/src/app/pages/hire-model/talent-item/talent-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'talent-item',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TalentItemComponent implements OnInit {
 
+  @Input() talent: any = {};
+  @Output() confirmHiring: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onConfirmHiring() {
+    this.confirmHiring.emit();
   }
 
 }
