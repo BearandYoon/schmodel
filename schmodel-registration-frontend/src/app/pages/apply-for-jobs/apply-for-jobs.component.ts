4ee0e95abfcb5e5aec942dcb6d70b6a5cf158881
BearandYoon <luckysevenbear@gmail.com>
2017-12-08 02:35:14 +1000

    fixed the date format in apply-for-jobs page

--- schmodel-registration-frontend/src/app/pages/apply-for-jobs/apply-for-jobs.component.ts
+++ schmodel-registration-frontend/src/app/pages/apply-for-jobs/apply-for-jobs.component.ts
@@ -56,6 +56,7 @@ export class ApplyForJobsComponent implements OnInit {
         this.jobService.getApplyForJobs((success, response) => {
             if (success) {
                 this.eventView = response;
+                console.log(response);
             }
             else {
                 this.showFailLoadingDialog();
