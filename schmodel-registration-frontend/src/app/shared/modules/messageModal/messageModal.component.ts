e8da5d3f22b0d2f4bc91081946a3ad258e02c855
BearandYoon <luckysevenbear@gmail.com>
2017-12-13 12:04:14 +1000

    fixed the validation style of reset-token valid

--- schmodel-registration-frontend/src/app/shared/modules/messageModal/messageModal.component.ts
+++ schmodel-registration-frontend/src/app/shared/modules/messageModal/messageModal.component.ts
@@ -10,7 +10,7 @@ import { Subject } from 'rxjs/Subject';
 })
 export class MessageModalComponent implements OnInit {
   messageContent: String;
-  messageTitle: String;
+  messageTitle: String = "Error";
   isBtnCancel: boolean;
   public onCloseReason: Subject<string>;
 
