ae7419f33451ba05ab6a4a5e7d99605bbaac0d8a
BearandYoon <luckysevenbear@gmail.com>
2017-12-26 19:24:48 +0100

    #245-sticky-table..
    ## updated sticky header for event-calendar page

--- schmodel-registration-frontend/src/app/pages/event-calendar/event-calendar.component.ts
+++ schmodel-registration-frontend/src/app/pages/event-calendar/event-calendar.component.ts
@@ -25,7 +25,7 @@ export class EventCalendarComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
   onPageScroll(event) {
-      if(event.target.scrollTop > 60) {
+      if (event.target.scrollingElement.scrollTop > 60) {
           this.stickyFlag = true;
       } else {
           this.stickyFlag = false;
