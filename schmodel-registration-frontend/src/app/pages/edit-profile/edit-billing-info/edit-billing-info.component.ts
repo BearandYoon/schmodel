6c8f8ca82dd2bf1997e7085316f4c30b1b37dd48
BearandYoon <luckysevenbear@gmail.com>
2017-12-30 10:32:20 +0100

    #253-save-button-should-be-enabled-only-when-t..
    ## added save button enabled in billing info

--- schmodel-registration-frontend/src/app/pages/edit-profile/edit-billing-info/edit-billing-info.component.ts
+++ schmodel-registration-frontend/src/app/pages/edit-profile/edit-billing-info/edit-billing-info.component.ts
@@ -14,6 +14,7 @@ export class EditBillingInfoComponent implements OnInit {
   @Output() collapseSection: EventEmitter<any> = new EventEmitter();
   editBillingForm: FormGroup;
   status: any = null;
+  untouched = true;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -65,6 +66,7 @@ export class EditBillingInfoComponent implements OnInit {
 
   onChange(event: any) {
     this.status = null;
+    this.untouched = false;
   }
 
   onSubmit() {
