7f35ad0767bb477dd9bcd93a7c7392fc593171f1
BearandYoon <luckysevenbear@gmail.com>
2018-01-23 16:27:08 +0100

    Use web optimized background images

--- schmodel-registration-frontend/src/app/pages/event-calendar/event-calendar.component.ts
+++ schmodel-registration-frontend/src/app/pages/event-calendar/event-calendar.component.ts
@@ -13,13 +13,28 @@ export class EventCalendarComponent implements OnInit {
 
   stickyFlag = false;
   eventItems: any;
-  bgImgUrls: string[];
+  race_bg_urls: Array<any> = [];
+  bgImgUrls: Array<any> = [];
 
   constructor(
     public router: Router,
     private profileService: ProfileService,
-    private sharedService: SharedService
-  ) { }
+    private sharedService: SharedService,
+  ) {
+    this.race_bg_urls.push(
+      { imagePath: '/assets/img/bg_r1_2.png' },
+      { imagePath: '/assets/img/bg_r3.png' },
+      { imagePath: '/assets/img/bg_r4.png' },
+      { imagePath: '/assets/img/bg_r5.png' },
+      { imagePath: '/assets/img/bg_r6.png' },
+      { imagePath: '/assets/img/bg_r7.png' },
+      { imagePath: '/assets/img/bg_r8.png' },
+      { imagePath: '/assets/img/bg_r9.png' },
+      { imagePath: '/assets/img/bg_r10.png' },
+      { imagePath: '/assets/img/bg_r11_12.png' },
+      { imagePath: '/assets/img/bg_r13_14.png' }
+    );
+  }
 
   @HostListener('window:scroll', ['$event'])
   onPageScroll(event) {
@@ -35,16 +50,19 @@ export class EventCalendarComponent implements OnInit {
     window.scrollTo(scrollLeft, 0);
     this.profileService.getEventCalendar().subscribe(res => {
       this.eventItems = res;
+
+      // race background image urls
       this.bgImgUrls = [];
-      this.eventItems.eventList.map(x => {
-        this.bgImgUrls.push(this.getRandomImage());
+      this.eventItems.eventList.map((x, index) => {
+        this.bgImgUrls.push(this.getImageUrl(index));
       });
     });
   }
 
-  getRandomImage() {
-    const rand = Math.floor(Math.random() * 15);
-    return 'url(/assets/img/bg_eventcalendar' + rand + '.png)';
+  // race background image url
+  getImageUrl(ind) {
+    const index = ind % 11;
+    return 'url(' + this.race_bg_urls[index].imagePath + ')';
   }
 
   onEvent(i) {
