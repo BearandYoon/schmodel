891c5c7a0c256137d37943441eaba33b3d53b92a
BearandYoon <luckysevenbear@gmail.com>
2018-02-19 11:03:04 +0000

    Revert "Add build-backend.sh script for dev env with new maven profile."
    This reverts commit 57ce9e2456677d89d9b7c5f419ede511a43a33a2.

--- deploy/build-backend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -R ~/schmodel-registration-backend/schmodel-registration-backend
-git clone https://ec2-54-246-224-86.eu-west-1.compute.amazonaws.com/schmodel/schmodel-registration-backend.git
-cd schmodel-registration-backend/schmodel-registration-backend
-mvn install
